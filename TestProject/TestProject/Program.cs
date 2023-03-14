a8b21054be0d0cda9affde5d113d371baa9e74c3
Andrii Kud <Andrii.kud@ideo-digital.com>
2023-03-14 21:47:02 +0200

    json file fetched, gitignore file added.

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Create an HTTP request to a JSON API endpoint
+using System.Net;
+
+HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://pipes2.kan.org.il/api/item?tagId=137");
+request.Method = "GET";
+
+// Send the request and get the response
+HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+Stream dataStream = response.GetResponseStream();
+StreamReader reader = new StreamReader(dataStream);
+string responseFromServer = reader.ReadToEnd();
+
+// Write the response to a JSON file
+File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "response.json"), responseFromServer);
